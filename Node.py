8de4e40a21069ae3341f65d1047314e3e076868f
Lu Sun <sunlufinal@gmail.com>
2013-11-07 16:46:26 -0500

    start to use networkx package

--- Node.py
+++ Node.py
@@ -1,5 +1,14 @@
 class Node:
 
-	_id=0
+	_id=-1
 	_name=""
+
+	def get_name(self):
+		return self._name
+
+	def set_name(self,name):
+		self._name=name
+
+	def __init__(self,id):
+		_id=id
 		
--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+from networkx import nx
 from person import *
 from action import *
 
-a=Person()
-b=Person()
-a.attack_value=100
-b.defense_value=30
-attack(a,b)
-if(b.is_alive):
-	print("b is still alive")
-else:
-	print("b is dead")
-attack(a,b)
-if(b.is_alive):
-	print("b is still alive")
-else:
-	print("b is dead")
+# a=Person()
+# b=Person()
+# a.attack_value=100
+# b.defense_value=30
+# attack(a,b)
+# if(b.is_alive):
+# 	print("b is still alive")
+# else:
+# 	print("b is dead")
+# attack(a,b)
+# if(b.is_alive):
+# 	print("b is still alive")
+# else:
+# 	print("b is dead")
+
+###global initialization
+world=nx.Graph()
+next_id=1
+list_of_node=[]
+
+tmp=Person(next_id)
+next_id+=1
+list_of_node.append(tmp)
+
