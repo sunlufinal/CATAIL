765ac345d94cee7d2d0359fa7db0122589dbfc61
Lu Sun <sunlufinal@gmail.com>
2013-12-02 15:58:40 -0500

    none

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-class testClass:
-	"""this is a test class"""
-	a="abc"
-	def __init__(self):
-		print(self.a)
-	def print_a():
-		print("lalala")
+import networkx as nx
 
-a=testClass()
+class a:
+	value=5
+
+g=nx.Graph()
+g.add_node(1)
+g.add_node(2)
+att=a()
+att2=a()
+att2.value=6
+g.add_edge(1,2,attr=att)
+#g.add_edge(1,2,attr=att2)
+g.edges()
--- test2.py
+++
@@ -1,6 +0,0 @@
-def test(a):
-	a-=1
-
-v=5
-test(v)
-print(v)
